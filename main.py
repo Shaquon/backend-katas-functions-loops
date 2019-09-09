e1994ecd356efd07d3a452223b457c14663cbd08
Shaquon <shaquonkelley@gmail.com>
2019-09-09 10:41:42 -0400

    fixed all of the functions and tested each one with a print statement

--- main.py
+++ main.py
@@ -1,37 +1,51 @@
-def add(x,y):
-  return x+y
+def add(x, y):
+    return x+y
 
-def multiply(x,y):
-  up=0
-  for i in range(0,1):
-    up=add(up,y)
-  return up
+print(add(2,2))
 
-def power(x,n):
- top = 1
- for i in range(1,n):
-  multiply(top,x)
-  return
+def multiply(x, y):
+    up = 0
+    for i in range(0, x):
+        up = add(up, y)
+    return up
+
+print(multiply(2,3))
+
+def power(x, n):
+    top = 1
+    for i in range(0, n):
+        top = multiply(top, x)
+    return top
+
+print(power(5,2))
 
 def factorial(x):
-  tree = x
-  for i in range(2,x):
-    tree=multiply(tree,x-1)
-  return tree
+    tree = x
+    for i in range(2, x):
+        tree = multiply(tree, x-1)
+        x=x-1
+    return tree
+print(factorial(35))
+
 
 def fibonacci(n):
-  bus=0
-  lego = 0
-  bat =1
-  if n == 0:
-    return "Error"
-  elif n == 1:
-    return 0
-  elif n == 2:
-    return 1
-  else:
-    for k in range(0,n):
-      bus = add(lego, bat);
-      lego = bat
-      bat = bus
-  return bus
+    bus = 0
+    lego = 0
+    bat = 1
+    if n == 0:
+        return "Error"
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        for k in range(2, n):
+            bus = add(lego, bat)
+            lego = bat
+            bat = bus
+    return bus
+
+print(fibonacci(2))
+print(fibonacci(3))
+print (fibonacci(4))
+print(fibonacci(5))
